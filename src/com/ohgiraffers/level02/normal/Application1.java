c19c76cd5077eb01ba09f7e093d8429da82326c6
1bfish106 <1bfish106@naver.com>
2023-12-29 17:15:00 +0900

    FIRST COMMIT

---
+++ src/com/ohgiraffers/level02/normal/Application1.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.level02.normal;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+        char ch = 'a';
+        int ch1 = (int) ch;
+        System.out.println("문자 a의 unicode는 " + ch1 );
+    }
+}
