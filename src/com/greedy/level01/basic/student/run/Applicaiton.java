66d3c09bc1df122380093dcfb1a525d496f60ff9
1bfish106 <1bfish106@naver.com>
2024-01-11 09:20:58 +0900

    ddddd

--- src/com/greedy/level01/basic/student/run/Applicaiton.java
+++ src/com/greedy/level01/basic/student/run/Applicaiton.java
@@ -7,40 +7,47 @@ import java.util.Scanner;
 public class Applicaiton {
     public static void main(String[] args) {
         StudentDTO[] studentDTOS = new StudentDTO[10];
-
         int count = 0;
+
         Scanner sc = new Scanner(System.in);
+
         while (true) {
-            for (int i = 0; i < studentDTOS.length; i++) {
-                System.out.println("학년 : ");
-                int grade = sc.nextInt();
-                System.out.println("반 : ");
-                int classroom = sc.nextInt();
-                System.out.println("이름 : ");
-                String name = sc.nextLine();
+            System.out.println("학년 : ");
+            int grade = sc.nextInt();
+            System.out.println("반 : ");
+            int classroom = sc.nextInt();
+            sc.nextLine();
+            System.out.println("이름 : ");
+            String name = sc.nextLine();
 
-                sc.nextLine();
-                System.out.println("국어점수 : ");
-                int kor = sc.nextInt();
-                System.out.println("영어점수 : ");
-                int eng = sc.nextInt();
-                System.out.println("수학점수 : ");
-                int math = sc.nextInt();
+            System.out.println("국어점수 : ");
+            int kor = sc.nextInt();
+            System.out.println("영어점수 : ");
+            int eng = sc.nextInt();
+            System.out.println("수학점수 : ");
+            int math = sc.nextInt();
 
+            studentDTOS[count] = new StudentDTO(grade, classroom, name, kor, eng, math);
+            count++;
 
-                studentDTOS[count] = new StudentDTO(grade, classroom, name, kor, eng, math);
-                count++;
+            while (true) {
                 System.out.println("계속 추가할겁니까?");
                 char str = sc.next().charAt(0);
                 sc.nextLine();
-                if ( str == 'n' && count < 10 ) {
-                    System.out.println("----- 입력 생략 -----");
-                    System.out.println(studentDTOS[i].getInformation());
-                    return;
-                } else{
 
+                if (str == 'n' || count == 10) {
+                    System.out.println("=====입력생략=====");
+                    for (int i = 0; i < count; i++) {
+                        System.out.println(studentDTOS[i].getInformation());
+                        break;
+                    }
+                    return;
+                }
+                if (str == 'y') {
+                    break;
                 }
             }
         }
     }
 }
+
