b620865dcdb4cac79d3e32ba19bbe3e79d8f46aa
blimu <sheep.ohgiraffers@gmail.com>
2024-04-26 15:40:05 +0900

    commit

--- lecture-source/05_router/chap02-navbar/src/App.js
+++ lecture-source/05_router/chap02-navbar/src/App.js
@@ -1,6 +1,13 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./layouts/Layout";
+
 function App() {
   return (
-   <></>
+   <BrowserRouter>
+     <Routes>
+       <Route path="/" element={<Layout/> }/>
+     </Routes>
+   </BrowserRouter>
   );
 }
 
