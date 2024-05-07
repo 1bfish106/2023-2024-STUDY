198d864053854a9058c0750a47ebbc19b31db2d0
blimu <sheep.ohgiraffers@gmail.com>
2024-05-07 11:09:57 +0900

    commit

--- chap02-comprehensive/src/main/java/com/ohigraffers/comprehensive/product/presentation/ProductController.java
+++ chap02-comprehensive/src/main/java/com/ohigraffers/comprehensive/product/presentation/ProductController.java
@@ -1,5 +1,8 @@
 package com.ohigraffers.comprehensive.product.presentation;
 
+import com.ohigraffers.comprehensive.common.paging.Pagenation;
+import com.ohigraffers.comprehensive.common.paging.PagingButtonInfo;
+import com.ohigraffers.comprehensive.common.paging.PagingResponse;
 import com.ohigraffers.comprehensive.product.dto.response.CustomerProductsResponse;
 import com.ohigraffers.comprehensive.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -19,15 +22,17 @@ public class ProductController {
 
     /* 상품 목록 조회 (고객) */
     @GetMapping("/products")
-    public ResponseEntity<> getCustomerProducts(
+    public ResponseEntity<PagingResponse> getCustomerProducts(
             @RequestParam(defaultValue = "1") final Integer page,
             @RequestParam(required = false) final Long categoryCode,
             @RequestParam(required = false) final String productName
     ) {
 
         final Page<CustomerProductsResponse> products = productService.getCustomerProducts(page, categoryCode, productName);
+        final PagingButtonInfo pagingButtonInfo = Pagenation.getPagingButtonInfo(products);
+        final PagingResponse pagingResponse = PagingResponse.of(products.getContent(), pagingButtonInfo);
 
-        return ResponseEntity.ok();
+        return ResponseEntity.ok(pagingResponse);
     }
 
 
