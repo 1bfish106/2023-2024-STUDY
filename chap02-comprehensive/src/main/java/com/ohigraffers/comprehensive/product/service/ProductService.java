a850c3106280e4b03964df784c1e1ba982e5acdc
blimu <sheep.ohgiraffers@gmail.com>
2024-05-07 11:20:14 +0900

    commit

--- chap02-comprehensive/src/main/java/com/ohigraffers/comprehensive/product/service/ProductService.java
+++ chap02-comprehensive/src/main/java/com/ohigraffers/comprehensive/product/service/ProductService.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.print.attribute.standard.PageRanges;
 
 @Service
 @RequiredArgsConstructor
