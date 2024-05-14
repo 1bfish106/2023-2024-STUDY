598816cc56ced60c045370f350c1f00b53bb8ff1
blimu <sheep.ohgiraffers@gmail.com>
2024-05-14 13:15:31 +0900

    commit

--- chap02-comprehensive/src/main/java/com/ohgiraffers/comprehensive/order/domain/repository/OrderRepository.java
+++ chap02-comprehensive/src/main/java/com/ohgiraffers/comprehensive/order/domain/repository/OrderRepository.java
@@ -10,6 +10,9 @@ import org.springframework.data.jpa.repository.Query;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     boolean existsByOrderCodeAndMemberCode(Long orderCode, Long memberCode);
 
-    @Query("")
+    @Query("select new com.ohgiraffers.comprehensive.order.dto.response.OrdersResponse(o, p, r) "+
+            "from Order o join Product p on o.productCode = p.productCode " +
+            "left join Review r on o.orderCode = r.orderCode " +
+            "where o.memberCode = :memberCode")
     Page<OrdersResponse> findByMemberCode(Pageable pageable, Long memberCode);
 }
--- chap02-comprehensive/src/main/java/com/ohgiraffers/comprehensive/order/dto/response/OrdersResponse.java
+++ chap02-comprehensive/src/main/java/com/ohgiraffers/comprehensive/order/dto/response/OrdersResponse.java
@@ -1,6 +1,9 @@
 package com.ohgiraffers.comprehensive.order.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ohgiraffers.comprehensive.order.domain.entity.Order;
+import com.ohgiraffers.comprehensive.product.domain.entity.Product;
+import com.ohgiraffers.comprehensive.review.domain.entity.Review;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -18,7 +21,19 @@ public class OrdersResponse {
     private String reviewTitle;
     private String reviewContent;
 
-
+    public OrdersResponse(Order order, Product product, Review review) {
+        this.orderCode = order.getOrderCode();
+        this.productCode = order.getProductCode();
+        this.productName = product.getProductName();
+        this.productPrice = product.getProductPrice();
+        this.orderAmount = order.getOrderAmount();
+        this.orderDate = order.getOrderDate();
+        if(review != null){
+            this.reviewCode = review.getReviewCode();
+            this.reviewTitle = review.getReviewTitle();
+            this.reviewContent = review.getReviewContent();
+        }
+    }
 
 
 
